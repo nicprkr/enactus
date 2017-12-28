af7930d62f49f8c17145c9b371aa847f1c52a6ab
nicprkr <nicolarparker@gmail.com>
2017-12-28 12:36:24 -0800

    basic structure for home page

--- enactus-web/src/App.js
+++ enactus-web/src/App.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav.js';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+    }
   render() {
+      
     return (
       <div className="App">
+        <Nav />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Add Hours</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form className="Hours-form">
+        
+        <select name="project" placeholder="Select your project...">
+            <option value="Project" disabled="disabled">Select your project</option>
+            <option value="marketing">marketing</option>
+            <option value="teaching">teaching</option>
+            <option value="meetings">meetings</option>
+        </select>
+        
+        <br/><br/>
+            <input name="hours" type="number" min="0" max="8" placeholder="hours" />
+            <input name="mins" type="number" min="0" max="59" placeholder="minutes" />
+        <br/><br/>
+            <button>Add</button>
+        </form>
       </div>
     );
   }
